48e639e3cf6642be637a800538274963921ed395
kristijanxo <145933386+kristijanxo@users.noreply.github.com>
2025-09-09 16:03:57 +0200

    Implement getComputerChoice() function
    The function returns rock, paper or scissors as a string depending on
    which number gets rolled with Math.random

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+function getComputerChoice() {
+  let randNum = Math.floor(Math.random() * 3);
+  if (randNum === 0) {
+    return "rock";
+  } else if (randNum === 1) {
+    return "paper";
+  } else if (randNum === 2) {
+    return "scissors";
+  }
+}
